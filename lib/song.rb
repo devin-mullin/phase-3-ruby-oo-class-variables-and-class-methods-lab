695e427f9f33f53af88cb587b5813c684a57e956
Devin Mullin <devin.mullin90@gmail.com>
2021-10-14 17:35:05 -0500

    done

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+class Song
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artists, genres)
+        @name = name
+        @artists = artists
+        @genres = genres
+        @@count += 1
+        @@genres << @genres
+        @@artists << @artists
+    end 
+
+    @@count = 0 
+
+    @@genres = []
+
+    @@artists = []
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artists 
+        @@artists.uniq
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end 
+
+fade = Song.new("Fade", "Kanye West", "Hip-Hop")
+
